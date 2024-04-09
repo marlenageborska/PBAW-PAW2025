69342e275da56a5c28001b09cdecedcfcca7b7c1
Marlena <Marlena@DESKTOP-S5I6TSL>
2024-04-10 01:16:45 +0200

    First commit
    01_widok_kontroler

---
+++ 01_widok_kontroler/apP1calc.php
@@ -0,0 +1,71 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$kwota = $_REQUEST ['kwota'];
+$czas = $_REQUEST ['czas'];
+$oprocentowanie = $_REQUEST ['oprocentowanie'];
+
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($kwota) && isset($czas)&& isset($oprocentowanie) )) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $kwota == "") {
+	$messages [] = 'Nie podano kwoty kredytu';
+}
+if ( $czas == "") {
+	$messages [] = 'Nie podano liczby lat';
+}
+if ( $oprocentowanie == "") {
+	$messages [] = 'Nie podano wartości oprocentowania';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $kwota i $czas są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $czas )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}	
+	
+	if (! is_numeric( $oprocentowanie )) {
+		$messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
+	}
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int lub float
+	$kwota = intval($kwota);
+	$czas = intval($czas);
+	$oprocentowanie = floatval($oprocentowanie);
+	
+	//wykonanie operacji
+	$result = ($kwota / ($czas * 12)) + (($kwota ) * (($oprocentowanie /100)/12));
+	
+	
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$kwota,$czas,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'apP1calc_view.php';
---
+++ 01_widok_kontroler/apP1calc_view.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+</head>
+<body background='tlo.jpg'>
+
+<form action="<?php print(_APP_URL);?>/apP1calc.php" method="post">
+	<label for="id_kwota">Kwota kredytu </label>
+	<input id="id_kwota" type="text" name="kwota" value="<?php if(isset($kwota)) print($kwota); ?>" /><br />
+	
+	<label for="id_czas">Liczba lat </label>
+	<input id="id_czas" type="text" name="czas" value="<?php if(isset($czas)) print($czas); ?>" /><br />
+	
+	<label for="id_oprocentowanie">Oprocentowanie roczne % </label>
+	<input id="id_oprocentowanie" type="text" name="oprocentowanie" value="<?php if(isset($oprocentowanie)) print($oprocentowanie); ?>" /><br />
+	
+	<input type="submit" value="Oblicz ratę kredytu" />
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 30px; padding: 20px 20px 20px 40px; border-radius: 5px; background-color: #ff00ff; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Wynik: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
