859eb400b1483512832fbf6cf947bf1c374db9c7
Marlena <Marlena@DESKTOP-S5I6TSL>
2025-03-23 21:15:20 +0100

    zad.3

---
+++ 03_proste_szablonowanie/index.php
@@ -0,0 +1,12 @@
+
+<?php
+require_once dirname(__FILE__).'/config.php';
+
+//przekierowanie przeglądarki klienta (redirect)
+//header("Location: "._APP_URL."/app/apP1calc.php");
+
+//przekazanie żądania do następnego dokumentu ("forward")
+include _ROOT_PATH.'/app/apP1calc.php';
+
+?>
+
